943e248194a06cbf72ef87cc98bf4757dcc4ce6e
Arda Aşkın <askin.arda@outlook.com>
2023-04-01 02:44:07 +0300

    updated version

--- shiny/gene_vs_gene.R
+++ shiny/gene_vs_gene.R
@@ -76,15 +76,15 @@ gene_vs_gene_ui <- function(id) {
       actionButton(ns("intro"), "App Tutorial", style="color: #FFFFFF; background-color: #81A1C1; border-color: #02a9f7"),
       
       
-
+      
     ),
     
-   
+    
     
     mainPanel(
       girafeOutput(ns("plot"))
       
-      )
+    )
     
     
     
@@ -97,11 +97,11 @@ gene_vs_gene_server <- function(id,Xproj) {
     id,
     function(input, output, session) {
       
-     
       
-     
       
-       ## help section server
+      
+      
+      ## help section server
       
       ns <- session$ns
       
@@ -112,14 +112,14 @@ gene_vs_gene_server <- function(id,Xproj) {
           element = paste0("#", session$ns(c(NA, "genecor_samp + .selectize-control", "Gene1 + .selectize-control ", "Gene2+ .selectize-control ", "notification","genecor_regline", "facet", "formula"))),
           
           intro = paste(c(
-            "This is the Gene-to-Gene Correlation Analysis app, press the buttons to learn features of the app.",
-            "You can choose single tissue type or more than one tissue types to filter patients who have that tissue type.",
-            "Choose a gene for x-axis of the plot.",
-            "Choose a gene for y-axis of the plot.",
-            "Thanks to this button, you will see notification about patients that appear when you hover the mouse over any point",
-            "This button creates the regression line on the plot.",
-            "This button separates the main plot to subplots according to your clinical feature choice ",
-            "This button shows p and R values on the plot."
+            "This is the gene-to-gene visualization module. You can calculate the correlation between two genes and generate scatter plots. Continue with the tutorial to learn features of the module.",
+            "You can select the sample type here (eg. primary and/or metastatic). Only the selected subset(s) will be used in the analysis",
+            "Choose a gene for the x-axis of the plot.",
+            "Choose a gene for the y-axis of the plot.",
+            "Here, you can select whether or not to show associated metadata when you hover the mouse cursor over data points",
+            "You can show or hide the best-fitting line to the data points",
+            "Enter a covariate here to plot the correlations in different data subsets (eg. male and female patients).",
+            "You can also show the correlation coefficient and the p-value of the linear regression."
           ))
           
         )
@@ -133,7 +133,7 @@ gene_vs_gene_server <- function(id,Xproj) {
         
       })
       
-
+      
       
       observe({updateSelectizeInput(session, "genecor_samp",choices = Xproj$a()[["meta.definition"]], server = T)})
       observe({updateSelectizeInput(session, 'Gene1', choices = colnames(gene_choice()), server = TRUE, selected = "")})
@@ -161,14 +161,14 @@ gene_vs_gene_server <- function(id,Xproj) {
           select(where(is.numeric))
         
       })
-    
+      
       
       
       df <- reactive({Xproj$a()%>% 
           filter( meta.definition %in% input$genecor_samp) %>% 
           filter(!is.na(input$Gene1)) %>% 
           filter(!is.na(input$Gene2))
-        })
+      })
       
       
       
@@ -176,19 +176,16 @@ gene_vs_gene_server <- function(id,Xproj) {
       
       facet_cat <- reactive({paste("~", input$Facet)})
       
-      g_tit <- reactive({paste(input$Gene1, "vs", input$Gene2, " CORRELATION PLOT")})
-      
-      
       
       ## ggiraph package is used to form interactive graph from ggplot 2 
       
       
       
       observeEvent(input$do, {
-
         
         
-
+        
+        
         output$plot <- renderGirafe({
           
           
@@ -197,11 +194,11 @@ gene_vs_gene_server <- function(id,Xproj) {
           
           isolate({
             
-           validate(
-             need(input$genecor_samp, "Choose at least one sample type"),
-             need(input$Gene1, "Choose the first gene"),
-             need(input$Gene2, "Choose the second gene"),
-           )
+            validate(
+              need(input$genecor_samp, "Choose at least one sample type"),
+              need(input$Gene1, "Choose the first gene"),
+              need(input$Gene2, "Choose the second gene"),
+            )
             
             if (input$Facet < 0  && input$facet == T ) {
               validate("Choose a faceting parameter")
@@ -216,22 +213,18 @@ gene_vs_gene_server <- function(id,Xproj) {
                                                                                            "Patient ID: ", dataset[["meta.patient"]], "<br/>",
                                                                                            "Race: ", dataset[["meta.race"]]))}
             
-
-                                                                          
+            
+            
             {if(input$notification == F) p <- p + geom_point_interactive(aes(x = .data[[input$Gene1]], y = .data[[input$Gene2]]))}       
             
-            
-            
-             
-            p <- p + ggtitle(g_tit())
             
             {if(input$formula) p <- p + stat_cor(mapping = aes(x = .data[[input$Gene1]], y = .data[[input$Gene2]]), family = "Arial", size = 7, color = "black", geom = "label")}        
             
             {if(input$genecor_regline) p <- p + stat_smooth(mapping = aes(x = .data[[input$Gene1]], y = .data[[input$Gene2]]), method = "lm", color = "#ff5317")}   
             
             {if(input$facet) p <- p + facet_wrap(facet_cat(), labeller = as_labeller(dataset, default=label_wrap_gen(16)), scales = "free_y")+ 
-              
-                                       theme(aspect.ratio = 1)}   
+                
+                theme(aspect.ratio = 1)}   
             
             p <- p + theme_bw(base_size = 16)
             
@@ -250,26 +243,11 @@ gene_vs_gene_server <- function(id,Xproj) {
                    
             )
             
-            
-            
-            
-            
-            
           })
           
-          
-          
         })
-      
         
-        
-  })
-      
-      
-      
-      
-      
-      
+      })
       
     }
   )
