41aef44e00eab2ef6720e35027dbe65237f13de8
Muhammet Emre Kuş <muhammetkus@iyte.edu.tr>
2023-08-03 22:20:23 +0300

    corrections

--- shiny/col_type_assign.R
+++ shiny/col_type_assign.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(data.table)
 
 tcga_projects <- c("ACC","BLCA","CESC","CHOL","COAD",
                    "DLBC","ESCA","GBM","KICH","KIRP","LAML",
@@ -18,11 +19,29 @@ for (i in tcga_projects) {
   data <- readRDS(input_file)
   
   data <- data %>%
-    mutate(across(starts_with("meta."), ~replace(., tolower(.) %in% c("na", "-", "not available", "notavailable","NA","Not Available","not Available"), NA)))
+    mutate(across(starts_with("meta."), ~replace(., tolower(.) %in% c("not-reported","na", "-","not reported","Not Reported","Not Reported", "not available", "notavailable","NA","Not Available","not Available"), NA)))
   
   if (!dir.exists("na_free_and_correct_type_of_column_data")) {
     dir.create("na_free_and_correct_type_of_column_data")
   }
   
+  
+  # "meta." ile başlayan sütunları bulalım
+  meta_columns <- grep("^meta\\.", names(data))
+  
+  # Her bir "meta." ile başlayan sütunu işleyelim
+  for (col in meta_columns) {
+    # Sütundaki unique değerleri ve unique değer sayısını alalım
+    unique_values <- unique(data[[col]])
+    num_unique_values <- length(unique_values)
+    numeric_unique_values<- sum(is.numeric(data[[meta.ploidy]]))
+    
+    # Sütundaki unique değerler sayısal ise ve sayısı 15'ten büyükse "factor" yerine "numeric" yapalım
+    if (numeric_unique_values >= 15 && num_unique_values >= 15) {
+      data[[col]] <- as.numeric(data[[col]])
+    }
+  }
+  
   saveRDS(data, output_file)
 }
+
